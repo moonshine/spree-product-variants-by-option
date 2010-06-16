9186381b9e76635542a8d6a10a338e18074fdb40
Etienne van Tonder <etiennevt@gmail.com>
2010-06-16 12:39:42 +1000

    Turn off override by default

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ map.show_variant '/products/:id/option/:option', :controller => 'products', :act
 
 # These routes will override the standard products/index and root routes to display
 # all variants in the system instead of all the products
-map.root :controller => "products", :action => "show_all_variants"
-map.show_all_variants "products/index", :controller => "products", :action => "show_all_variants"
+#map.root :controller => "products", :action => "show_all_variants"
+#map.show_all_variants "products/index", :controller => "products", :action => "show_all_variants"
 
